063b5f3a5e1abd819a383ab898a096b12c92e61c
Kelan Mazey <kelanmazey@localhost.localdomain>
2020-02-15 16:23:13 +0000

    added functionality to the buttons

--- part1Exercises/unicafe/src/index.js
+++ part1Exercises/unicafe/src/index.js
@@ -7,33 +7,43 @@ const Headings = ({text}) => <h1>{text}</h1>
 
 //Buttons
 const Button = ({ onClick, text }) => {
-    <button onClick={onClick}>
-        {text}
-    </button>
+    return (
+        <button onClick={onClick}>
+            {text}
+        </button>
+    )
 }
 //Displays
 const Display = ({ text, value }) => <p>{text} {value}</p>
-
-
-
 
 
 const App = () => {
     //save clicks of each button to own state
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
-    const [bad, setNBad] = useState(0)
+    const [bad, setBad] = useState(0)
+
+
+    const handleGoodClick = () => {
+        setGood(good + 1)
+    }
+    const handleNeutralClick = () => {
+        setNeutral(neutral + 1)
+    }
+    const handleBadClick = () => {
+        setBad(bad + 1)
+    }
 
     return (
         <div>
             <Headings text='give feedback'/>
-            <Button onClick={handleGoodClick} />
-            <Button onClick={handleBadClick} />
-            <Button onClick={handleNeutralClick} />
+            <Button onClick={handleGoodClick} text='good' />
+            <Button onClick={handleNeutralClick} text='neutral'/>
+            <Button onClick={handleBadClick} text='bad'/>
             <Headings text='statistics' />
             <Display text='good' value={good} />
-            <Display text='good' value={good} />
-            <Display text='good' value={good} />
+            <Display text='neutral' value={neutral} />
+            <Display text='bad' value={bad} />
         </div>
     )
 }
