e186e8814a43ae3c91791e52a2b542771309982e
Kelan Mazey <kelanmazey@localhost.localdomain>
2020-02-15 18:03:01 +0000

    added extra stats. Total, average and percent

--- part1Exercises/unicafe/src/index.js
+++ part1Exercises/unicafe/src/index.js
@@ -14,7 +14,15 @@ const Button = ({ onClick, text }) => {
     )
 }
 //Displays
-const Display = ({ text, value }) => <p>{text} {value}</p>
+const Display = ({ text, value }) => {
+        return (
+            <div>
+                <p>{text} {value}</p>
+            </div>
+        )
+    
+}
+
 
 
 const App = () => {
@@ -22,18 +30,26 @@ const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
+    const [all, setAll] = useState(0)
 
-
+    //handlers for button clicks
     const handleGoodClick = () => {
         setGood(good + 1)
+        setAll(all + 1)
     }
     const handleNeutralClick = () => {
         setNeutral(neutral + 1)
+        setAll(all + 1)
     }
     const handleBadClick = () => {
         setBad(bad + 1)
+        setAll(all + 1)
     }
 
+    const percent = (good - bad)/all;
+    const average = good/all;
+
+   //Render buttons and states, account for NaN with percent and average
     return (
         <div>
             <Headings text='give feedback'/>
@@ -44,6 +60,10 @@ const App = () => {
             <Display text='good' value={good} />
             <Display text='neutral' value={neutral} />
             <Display text='bad' value={bad} />
+            <Display text='all' value={all} />
+            <Display text='average' value={isNaN(percent) ? 0 : (good - bad)/all} />
+            <Display text='percentage' value={isNaN(average) ? '0 %' : (good/all)*100 + ' %'} />
+            
         </div>
     )
 }
