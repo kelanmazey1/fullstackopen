8c32f8bd4fcc1cc231c9e5a473ad84b862db8126
Kelan Mazey <kelanmazey@localhost.localdomain>
2020-03-01 08:24:34 +0000

    added function to add names

--- part2Exercises/phonebook/src/App.js
+++ part2Exercises/phonebook/src/App.js
@@ -1,23 +1,62 @@
 import React, {useState} from 'react';
+import Entry from './components/entry'
+
 
 const App = () => {
-  const [person, setPersons] = useState([
-    {name: 'Arto Hellas'}
+  const [persons, setPersons] = useState([
+    {name: 'Arto Hellas',
+     id: 'Arto Hellas'}
   ])
   const [newName, setNewName] = useState('')
+
+
+  const entries = () =>
+    persons.map(person =>
+      <Entry 
+        key={person.id}
+        person={person}
+      />
+    )
+
+
+  const addName = (event) => {
+    event.preventDefault()
+    
+
+    console.log(persons.map(person => person.name))
+    const personObject = {
+      name : newName,
+      id: newName
+    }
+    //if find returns an object, then there must be a match 
+    typeof (persons.find(({ name }) => name === personObject.name)) === 'object'
+    ? window.alert( `${personObject.name} is already in the phonebook!`)
+    : setPersons(persons.concat(personObject))
+    
+    console.log(persons)
+
+    setNewName('')
+      
+      
+  }
+  
+  const handleAddName = (event) => {
+    setNewName(event.target.value)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleAddName}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
+      <div>{entries()}</div>
     </div>
   )
 }
