59843a2ac53265ed014bd277396abf9100fb1567
Kelan Mazey <kelanmazey1@gmail.com>
2020-03-08 13:24:48 +0000

    added show function

--- part2Exercises/datafromcountries/src/App.js
+++ part2Exercises/datafromcountries/src/App.js
@@ -1,80 +1,119 @@
 import React, { useState, useEffect } from 'react';
 import './App.css'
-import Display from './components/viewSimple'
-import ViewDetailed from './components/viewDetailed'
-// import './App.css';
+// import View from './components/View'
+// import ViewDetailed from './components/viewDetailed';
 
 import axios from 'axios';
-
 
 
 const App = () => {
   const [search, setSearch] = useState('')
   const [countries, setCountries] = useState([])
- 
-
+  
+  const [view, setView] = useState('')
+  const [countryDisplay, setCountryDisplay] = useState([])
+  
   const countriesAll = () => {
     axios
     .get('https://restcountries.eu/rest/v2/all')
     .then(response => {
       // console.log(response.data)
       setCountries(response.data)
-       
+      setCountryDisplay(response.data) 
     })
   }
   
   useEffect(countriesAll, [])
 
-  const searchResults = countries
-    .filter(country => 
-      country
-      .name
-      .toLowerCase()
-      .includes(search.toLowerCase())
+  const display = () => {
+    if (view === 'detail') {
+      return (
+        countryDisplay.map((country, index) =>
+          <div key={index}>
+            <h2>{country.name}</h2>
+            <div>
+                <p>capital {country.capital}</p>
+                <p>population {country.population}</p>
+            </div>
+            <h3>languages</h3>
+            <div>
+                {country.languages.map((language, index) =>
+                    <li key={index}>{language.name}</li>)}
+            </div>
+            <div>
+                <img src={country.flag} alt='Countries flag'/>
+            </div>
+          </div>
+        )
       )
-
-  // const display = () =>
-  //   searchResults.map((country, index) =>
-  //       <Display 
-  //         key = {index}
-  //         country = {country} 
-  //         results = {searchResults}
-  //       />
-  //   )
-
-
-  
-  const displayHandler = (searchResults) => {
-    if (searchResults.length === 1) { 
-      return (searchResults.map((country, index) => 
-      <ViewDetailed
-        key={index} 
-        country={country}
-      /> ))
-      
-    } else if (searchResults.length <=10) {
-      return (searchResults.map((country, index) =>
-        <Display 
-          key = {index}
-          country = {country} 
-        />)
-    )
+    } else 
+    if (view === 'simple') {
+      return(
+        countryDisplay.map((country, index) =>
+          <div key={index}>
+            {country.name} <button onClick={() => handleButtonClick(country)}>Show</button>
+          </div>)
+      )
     } else {
-      return <div>Too many results, please specify a different filter</div>
+      return(
+        <div>Oogaly boogaly</div>
+      )
     }
-  }  
+  }
+  
+  const changeDetail = (results) => {
+    if (results.length === 1) {
+      setView('detail')
+      console.log('detailed')
+    } else if (results.length <= 10) {
+      setView('simple')
+      console.log('simple')
+    } else {
+      setView('default')
+      console.log('default')
+    }
+  }
+  //changes whether or not detailed view of country is shown
+ 
+  //handle button clic
+    
 
-  const handleSearchChange = (event) => {
-    setSearch(event.target.value)
+  const handleSearchChange = (event) => { 
+    const value = event.target.value
+    const results = countries
+    .filter(country => 
+    country
+    .name
+    .toLowerCase()
+    .includes(value.toLowerCase()))
+
+    setSearch(value)
+    changeDetail(results)
+    setCountryDisplay(results)  
+  
   }
 
-  console.log(searchResults)
+  const handleButtonClick = (country) => {
+
+      setView('detail')
+      setCountryDisplay([country])
+        
+  }
+
+  console.log('countryDisplay', countryDisplay)
+  console.log('view', view)
   return (
     <div className="App">
+      
+      <div>{countryDisplay.length}</div>
+      {/* <div>{searchResults.length}</div> */}
       <div>
         find countries <input value={search}  onChange={handleSearchChange}/>
       </div>
-      <div>{displayHandler(searchResults)}</div>
+      <div>
+        {display()}
+      </div>
+        
     </div>
   );
 }
