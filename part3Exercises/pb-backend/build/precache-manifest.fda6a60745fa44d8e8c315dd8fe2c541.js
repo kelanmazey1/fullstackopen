5835788eb408442a52e6bd46bbdcb9d298e7dfab
Kelan Mazey <kelanmazey1@gmail.com>
2020-03-23 20:30:53 +0000

    added procfile updated build

---
+++ part3Exercises/pb-backend/build/precache-manifest.fda6a60745fa44d8e8c315dd8fe2c541.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30fff950ca08934c43dd9a6e9267f4ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1838695393e9fae8b4a8",
+    "url": "/static/css/main.53ed6707.chunk.css"
+  },
+  {
+    "revision": "8f96a5a984918b8beff9",
+    "url": "/static/js/2.c3fa7b3e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.c3fa7b3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1838695393e9fae8b4a8",
+    "url": "/static/js/main.6895e000.chunk.js"
+  },
+  {
+    "revision": "73cc5c71f716e545cadb",
+    "url": "/static/js/runtime-main.a70bf60b.js"
+  }
+]);
