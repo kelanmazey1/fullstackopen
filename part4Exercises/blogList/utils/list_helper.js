2783b99fb216511b7394cf531e08b154f3143c00
Kelan Mazey <kelanmazey1@gmail.com>
2020-04-08 11:49:36 +0100

    completed part 4.7

--- part4Exercises/blogList/utils/list_helper.js
+++ part4Exercises/blogList/utils/list_helper.js
@@ -1,7 +1,25 @@
-const dummy = (blogs) => {
-  return 1;
+const dummy = (blogs) => 1;
+
+const totalLikes = (blogs) => {
+  const reducer = (likes, blog) => likes + blog.likes;
+
+  return blogs.reduce(reducer, 0);
+};
+
+const favouriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null;
+  }
+
+  const reducer = (previous, current) => (previous.likes > current.likes
+    ? previous
+    : current);
+
+  return blogs.reduce(reducer);
 };
 
 module.exports = {
   dummy,
+  totalLikes,
+  favouriteBlog,
 };
