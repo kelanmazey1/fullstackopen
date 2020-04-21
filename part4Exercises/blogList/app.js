4f2c81ebaa9a5d91b89fd054b5252fa0157a327c
Kelan Mazey <kelanmazey1@gmail.com>
2020-04-21 16:18:54 +0100

    Completed exercise 4.15

--- part4Exercises/blogList/app.js
+++ part4Exercises/blogList/app.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 
 const blogRouter = require('./controllers/blogs');
+const usersRouter = require('./controllers/users');
 const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
 const config = require('./utils/config');
@@ -30,6 +31,7 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 
 app.use('/api/blogs', blogRouter);
+app.use('/api/users', usersRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
