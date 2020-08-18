5a66262c6de394a4a15ca236c8fc8a5874c7d719
kelanmazey1 <kelanmazey1@gmail.com>
2020-08-18 16:19:59 +0100

    completed 9.15

--- part9Exercises/courseinfo-ts/src/index.tsx
+++ part9Exercises/courseinfo-ts/src/index.tsx
@@ -6,11 +6,54 @@ interface HeaderProps {
 }
 
 interface ContentProps {
-  parts: Array<{name: string, exerciseCount: number}>;
+  parts: Array<CoursePart>;
 }
+
+interface PartProps {
+  part: CoursePart;
+}
+
+  interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+}
+
+interface CoursePartDescription extends CoursePartBase {
+  description: string;
+}
+
+interface CoursePartOne extends CoursePartDescription {
+  name: "Fundamentals";
+}
+
+interface CoursePartTwo extends CoursePartBase {
+  name: "Using props to pass data";
+  groupProjectCount: number;
+}
+
+interface CoursePartThree extends CoursePartDescription {
+  name: "Deeper type usage";
+  exerciseSubmissionLink: string;
+}
+
+type CoursePart = CoursePartOne | CoursePartTwo | CoursePartThree;
 
 const Header: React.FC<HeaderProps> = ({ name }) => {
   return <h1>{name}</h1>;
+};
+
+const assertNever = (value: never): never => { throw new Error('Something went wrong') }
+
+const Part: React.FC<PartProps> = ({ part }) => {
+    switch (part.name) {
+      case "Fundamentals":
+        return <p>{part.name} {part.description} {part.exerciseCount}</p>
+      case "Using props to pass data":
+        return <p>{part.name} {part.exerciseCount} {part.groupProjectCount}</p>
+      case "Deeper type usage":
+        return <p>{part.name} {part.description} {part.exerciseCount} {part.exerciseSubmissionLink}</p>
+    }
+    return assertNever(part);
 };
 
 const Content: React.FC<ContentProps> = ({ parts }) => {
@@ -18,7 +61,7 @@ const Content: React.FC<ContentProps> = ({ parts }) => {
     <div>
       {
         parts.map((part) =>
-          <p key={part.name}>{part.name} {part.exerciseCount}</p>
+          <Part part={part} />
         )
       }
     </div>
@@ -33,21 +76,27 @@ const Total: React.FC<ContentProps> = ({ parts }) => {
     </p>)
 };
 
-
 const App: React.FC = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+
+
+  // this is the new coursePart variable
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is an awesome course part"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: "Confusing description",
+      exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev"
     }
   ];
 
